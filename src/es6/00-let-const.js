d977b1e44787ef05f6e37f7d89aacd00e1d0db32
jesusv916 <jevamon916@hotmail.com>
2023-02-13 21:56:25 -0500

    understanding some bases of es6

---
+++ src/es6/00-let-const.js
@@ -0,0 +1,24 @@
+var lastName = "Eduardo";
+lastName = "Jesus";
+console.log(lastName);
+
+let fruit = 'apple';
+fruit = 'kiwi';
+console.log(fruit);
+
+const animal = 'Dog';
+animal = 'cat';
+console.log(animal);
+
+const fruits = () => {
+    if(true){
+        var fruit1 = 'apple';
+        let fruit2 = 'kiwi';
+        const fruit3 = 'banana';
+    }
+    console.log(fruit1);
+    console.log(fruit2);
+    console.log(fruit3);
+}
+
+fruits();
