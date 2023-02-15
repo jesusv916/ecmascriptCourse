9697ba7ac7640c13e24b99bf3a45ec802deff4c9
jesusv916 <jevamon916@hotmail.com>
2023-02-14 20:50:15 -0500

    understanding how deconstructing works

---
+++ src/es6/04-rest-spread.js
@@ -0,0 +1,13 @@
+//arrays destructuring
+
+let fruits = ['apple', 'banana'];
+let [a, b] = fruits;
+
+console.log(a, b);
+
+//object destructuring
+
+let user = {username: 'Jesus', age: '22'};
+
+let {username, age} = user;
+console.log(username, age);
