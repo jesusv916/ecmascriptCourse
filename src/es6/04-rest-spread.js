ab9db418225875eceb55d04570ac4229b31d8b6c
jesusv916 <jevamon916@hotmail.com>
2023-02-14 21:12:04 -0500

    understanding the spread operator and rest

--- src/es6/04-rest-spread.js
+++ src/es6/04-rest-spread.js
@@ -11,3 +11,21 @@ let user = {username: 'Jesus', age: '22'};
 
 let {username, age} = user;
 console.log(username, age);
+
+//spread operator
+
+let person = {name: 'Angelie', age: '23'}
+let country = 'CO';
+
+let data = {...person, country}
+console.log(data);
+
+//rest
+
+function sum(num, ...values){
+    console.log(values);
+    console.log(num + values[0]);
+    return num + values[0];
+}
+
+sum(1, 1, 2, 3);
